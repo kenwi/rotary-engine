551d55ba816155935f81b47b6488f17c7236c2fd
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-08-13 22:45:21 +0200

    Refactoring

--- Engine/GameStates/Worlds/BaseWorld.cs
+++ Engine/GameStates/Worlds/BaseWorld.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Engine.Graphics;
 using Engine.Interfaces;
 using Engine.Managers;
@@ -10,13 +11,64 @@ namespace Engine.GameStates.Worlds
 {
     public abstract class BaseWorld : BaseGameState, IWorld
     {
+        private readonly List<Keyboard.Key> _keyDown;
+        private readonly List<Mouse.Button> _mouseButtonDown;
+        private Vector2i _mousePreviousPosition;
+        private WorldState _state = WorldState.Default;
+
+        internal TileMap Map;
+        internal Vector2i MouseDeltaVelocity;
+        internal int Width, Height, GridSize;
+        internal float ZoomLevel;
+        internal RenderWindow Window;
+
+        internal BaseWorld()
+        {
+            _mouseButtonDown = new List<Mouse.Button>();
+            _keyDown = new List<Keyboard.Key>();
+            ZoomLevel = 1;
+            Width = 128;
+            Height = 128;
+            GridSize = 64;
+        }
+
         public abstract void Update(RenderWindow target, float deltaTime);
-        public abstract void KeyPressed(RenderWindow target, object sender, KeyEventArgs e);
-        public abstract void KeyReleased(RenderWindow target, object sender, KeyEventArgs e);
-        public abstract void MousePressed(RenderWindow window, object sender, MouseButtonEventArgs e);
-        public abstract void MouseReleased(RenderWindow window, object sender, MouseButtonEventArgs e);
-        public abstract void MouseMoved(RenderWindow window, object sender, MouseMoveEventArgs e);
-        public abstract void MouseWheelScrolled(RenderWindow window, object sender, MouseWheelScrollEventArgs e);
+
+        public virtual void KeyPressed(RenderWindow target, object sender, KeyEventArgs e)
+        {
+            if (!_keyDown.Contains(e.Code))
+                _keyDown.Add(e.Code);
+        }
+
+        public virtual void KeyReleased(RenderWindow target, object sender, KeyEventArgs e)
+        {
+            _keyDown.Remove(e.Code);
+        }
+
+        public virtual void MousePressed(RenderWindow window, object sender, MouseButtonEventArgs e)
+        {
+            if (!_mouseButtonDown.Contains(e.Button))
+                _mouseButtonDown.Add(e.Button);
+        }
+
+        public virtual void MouseReleased(RenderWindow window, object sender, MouseButtonEventArgs e)
+        {
+            _mouseButtonDown.Remove(e.Button);
+        }
+
+        public virtual void MouseMoved(RenderWindow window, object sender, MouseMoveEventArgs e)
+        {
+            var currentPosition = new Vector2i(e.X, e.Y);
+            MouseDeltaVelocity = _mousePreviousPosition - currentPosition;
+            _mousePreviousPosition = currentPosition;
+        }
+
+        public virtual void MouseWheelScrolled(RenderWindow window, object sender, MouseWheelScrollEventArgs e)
+        {
+            var zoomLevelDelta = Math.Abs(e.Delta - 1) < 1.0 ? 0.5f : 2f;
+            ZoomWindow(zoomLevelDelta);
+        }
+
         public event EventHandler<WorldState> WorldStateChanged;
 
         public WorldState WorldState
@@ -27,38 +79,6 @@ namespace Engine.GameStates.Worlds
                 _state = value;
                 WorldStateChanged?.Invoke(this, WorldState);
             }
-        }
-
-        internal TileMap Map;
-        internal RenderWindow Window;
-        internal int Width, Height, GridSize;
-
-        private WorldState _state = WorldState.Default;
-        private float _zoomLevel;
-
-        internal BaseWorld()
-        {
-            _zoomLevel = 1;
-            Width = 128;
-            Height = 128;
-            GridSize = 64;
-        }
-
-        internal void MoveWindow(Vector2f direction)
-        {
-            var view = Window.GetView();
-            var newPosition = new Vector2f(direction.X * _zoomLevel, direction.Y * _zoomLevel);
-            view.Move(newPosition);
-            Window.SetView(view);
-        }
-
-        internal void ZoomWindow(float factor)
-        {
-            var view = Window.GetView();
-
-            _zoomLevel *= factor;
-            view.Zoom(factor);
-            Window.SetView(view);
         }
 
         public virtual void Initialize(RenderWindow target)
@@ -72,6 +92,33 @@ namespace Engine.GameStates.Worlds
         {
             if (WorldState != WorldState.Default) return;
             Map.Draw(target);
+        }
+
+        public virtual bool MouseDownAndHolding(Mouse.Button e)
+        {
+            return _mouseButtonDown.Contains(e);
+        }
+
+        public virtual bool KeyPressedAndHolding(Keyboard.Key e)
+        {
+            return _keyDown.Contains(e);
+        }
+
+        internal void MoveWindow(Vector2f direction)
+        {
+            var view = Window.GetView();
+            var newPosition = new Vector2f(direction.X * ZoomLevel, direction.Y * ZoomLevel);
+            view.Move(newPosition);
+            MouseDeltaVelocity = new Vector2i();
+            Window.SetView(view);
+        }
+
+        internal void ZoomWindow(float factor)
+        {
+            var view = Window.GetView();
+            ZoomLevel *= factor;
+            view.Zoom(factor);
+            Window.SetView(view);
         }
     }
 }
--- Engine/GameStates/Worlds/World01.cs
+++ Engine/GameStates/Worlds/World01.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using Engine.Graphics;
 using Engine.Managers;
 using SFML.Graphics;
@@ -11,101 +9,58 @@ namespace Engine.GameStates.Worlds
     public class World01 : BaseWorld
     {
         private Forest _forest;
-        private List<Keyboard.Key> _keyDown;
-        private List<Mouse.Button> _mouseButtonDown;
-        private Vector2i _mouseDeltaVelocity;
-        private Vector2i _mousePreviousPosition;
         private Player _player;
+        private int _playerMovementSpeed;
 
         public override void Initialize(RenderWindow target)
         {
-            _keyDown = new List<Keyboard.Key>();
-            _mouseButtonDown = new List<Mouse.Button>();
-            _player = new Player(new Vector2i(8, 5), 64);
+            base.Initialize(target);
 
+            _playerMovementSpeed = 200;
+            _player = new Player(new Vector2i(8, 5), 64);
             var treeTexture = AssetManager.Instance.Texture.Get(AssetManagerItemName.TreeTexture);
             _forest = new Forest(treeTexture, GridSize, Width, Height, 0.1);
-
-            base.Initialize(target);
-        }
-
-        private void UpdateCamera(float deltaTime)
-        {
-            const int speed = 200;
-            if (KeyPressedAndHolding(Keyboard.Key.Left)) MoveWindow(new Vector2f(-speed * deltaTime, 0));
-            if (KeyPressedAndHolding(Keyboard.Key.Right)) MoveWindow(new Vector2f(speed * deltaTime, 0));
-            if (KeyPressedAndHolding(Keyboard.Key.Up)) MoveWindow(new Vector2f(0, -speed * deltaTime));
-            if (KeyPressedAndHolding(Keyboard.Key.Down)) MoveWindow(new Vector2f(0, speed * deltaTime));
         }
 
         public override void Update(RenderWindow target, float deltaTime)
         {
-            const int speed = 200;
-            UpdateCamera(deltaTime);
-            if (KeyPressedAndHolding(Keyboard.Key.S)) _player.MovePosition(new Vector2f(0, speed * deltaTime));
-            if (KeyPressedAndHolding(Keyboard.Key.W)) _player.MovePosition(new Vector2f(0, -speed * deltaTime));
-            if (KeyPressedAndHolding(Keyboard.Key.D)) _player.MovePosition(new Vector2f(speed * deltaTime, 0));
-            if (KeyPressedAndHolding(Keyboard.Key.A)) _player.MovePosition(new Vector2f(-speed * deltaTime, 0));
+            CheckCameraInput(deltaTime);
+            CheckPlayerInput(deltaTime);
         }
 
         public override void Draw(RenderWindow target)
         {
             base.Draw(target);
+
             _player.Draw(target);
             _forest.Draw(target);
         }
 
-        public override void KeyPressed(RenderWindow target, object sender, KeyEventArgs e)
+        private void CheckCameraInput(float deltaTime)
         {
-            if (!_keyDown.Contains(e.Code))
-                _keyDown.Add(e.Code);
+            if (KeyPressedAndHolding(Keyboard.Key.Left))
+                MoveWindow(new Vector2f(-_playerMovementSpeed * deltaTime, 0));
+            if (KeyPressedAndHolding(Keyboard.Key.Right))
+                MoveWindow(new Vector2f(_playerMovementSpeed * deltaTime, 0));
+            if (KeyPressedAndHolding(Keyboard.Key.Up))
+                MoveWindow(new Vector2f(0, -_playerMovementSpeed * deltaTime));
+            if (KeyPressedAndHolding(Keyboard.Key.Down))
+                MoveWindow(new Vector2f(0, _playerMovementSpeed * deltaTime));
 
-            if (e.Code != Keyboard.Key.Comma && e.Code != Keyboard.Key.Period) return;
-
-            var zoomLevelDelta = e.Code == Keyboard.Key.Comma ? 0.5f : 2f;
-            ZoomWindow(zoomLevelDelta);
+            if (MouseDownAndHolding(Mouse.Button.Left))
+                MoveWindow(new Vector2f(MouseDeltaVelocity.X, MouseDeltaVelocity.Y));
         }
 
-        private bool KeyPressedAndHolding(Keyboard.Key e)
+        private void CheckPlayerInput(float deltaTime)
         {
-            return _keyDown.Contains(e);
-        }
-
-        private bool MouseDown(Mouse.Button e)
-        {
-            return _mouseButtonDown.Contains(e);
-        }
-
-        public override void MouseReleased(RenderWindow window, object sender, MouseButtonEventArgs e)
-        {
-            _mouseButtonDown.Remove(e.Button);
-        }
-
-        public override void KeyReleased(RenderWindow target, object sender, KeyEventArgs e)
-        {
-            _keyDown.Remove(e.Code);
-        }
-
-        public override void MousePressed(RenderWindow window, object sender, MouseButtonEventArgs e)
-        {
-            if (!_mouseButtonDown.Contains(e.Button))
-                _mouseButtonDown.Add(e.Button);
-        }
-
-        public override void MouseMoved(RenderWindow window, object sender, MouseMoveEventArgs e)
-        {
-            var currentPosition = new Vector2i(e.X, e.Y);
-            _mouseDeltaVelocity = _mousePreviousPosition - currentPosition;
-            _mousePreviousPosition = currentPosition;
-
-            if (MouseDown(Mouse.Button.Left))
-                MoveWindow(new Vector2f(_mouseDeltaVelocity.X, _mouseDeltaVelocity.Y));
-        }
-
-        public override void MouseWheelScrolled(RenderWindow window, object sender, MouseWheelScrollEventArgs e)
-        {
-            var zoomLevelDelta = Math.Abs(e.Delta - 1) < 1.0 ? 0.5f : 2f;
-            ZoomWindow(zoomLevelDelta);
+            if (KeyPressedAndHolding(Keyboard.Key.S))
+                _player.MovePosition(new Vector2f(0, _playerMovementSpeed * deltaTime));
+            if (KeyPressedAndHolding(Keyboard.Key.W))
+                _player.MovePosition(new Vector2f(0, -_playerMovementSpeed * deltaTime));
+            if (KeyPressedAndHolding(Keyboard.Key.D))
+                _player.MovePosition(new Vector2f(_playerMovementSpeed * deltaTime, 0));
+            if (KeyPressedAndHolding(Keyboard.Key.A))
+                _player.MovePosition(new Vector2f(-_playerMovementSpeed * deltaTime, 0));
         }
     }
 }
