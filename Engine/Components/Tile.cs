80fafcccbff2259df7a2bfd2e2c420f7271dc3db
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-08-19 22:44:52 +0200

    Adding rendersystem

--- Engine/Components/Tile.cs
+++ Engine/Components/Tile.cs
@@ -2,7 +2,7 @@ using SFML.Graphics;
 
 namespace Engine.Components
 {
-    struct Tile
+    public struct Tile
     {
         public uint Index;
         public uint Tu;
@@ -12,5 +12,22 @@ namespace Engine.Components
         public Vertex Vertex2;
         public Vertex Vertex3;
         public Vertex Vertex4;
+
+        public Tile(uint index) : this()
+        {
+            Index = index;
+        }
+
+        public Tile(uint index, uint tu, uint tv, uint tileNumber, Vertex vertex1, Vertex vertex2, Vertex vertex3, Vertex vertex4)
+        {
+            Index = index;
+            Tu = tu;
+            Tv = tv;
+            TileNumber = tileNumber;
+            Vertex1 = vertex1;
+            Vertex2 = vertex2;
+            Vertex3 = vertex3;
+            Vertex4 = vertex4;
+        }
     }
 }
--- Engine/Systems/RenderSystem.cs
+++ Engine/Systems/RenderSystem.cs
@@ -1,44 +1,73 @@
 ﻿using System;
+using DefaultEcs;
 using DefaultEcs.System;
+using Engine.Components;
 using SFML.Graphics;
 using SFML.System;
+using SFML.Window;
 
 namespace Engine.Systems
 {
-    public class RenderSystem : ISystem<RenderWindow>
+    public class RenderSystem : ISystem<(RenderTarget target, RenderStates state)>
     {
-        public bool IsEnabled { get; set; } = true;
-
         private bool _initialized;
         private int _frameCount;
         private Clock _outputTimer;
         private Time _timeLimit;
+        private World _world;
+        private Window _window;
+        private uint _width;
+        private uint _height;
+        private VertexArray _vertices;
 
-        public void Update(RenderWindow state)
+        public RenderSystem(World world, Window window, uint width, uint height)
+        {
+            _world = world;
+            _window = window;
+            _width = width;
+            _height = height;
+        }
+
+        private void Initialize()
+        {
+            _timeLimit = Time.FromSeconds(1);
+            _outputTimer = new Clock();
+            _frameCount = 0;
+            _initialized = true;
+        }
+
+        public bool IsEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update((RenderTarget target, RenderStates state) drawInfo)
         {
             if (!_initialized)
                 Initialize();
+
+            CalculateFps();
+
+            ref var target = ref drawInfo.target;
+            ref var state = ref drawInfo.state;
+            var entities = _world.GetEntities().With<Tile>().Build();
+            foreach(var entity in entities.GetEntities())
+            {
+                var tile = entity.Get<Tile>();
+            }
+        }
+
+        private void CalculateFps()
+        {
             if (_outputTimer.ElapsedTime > _timeLimit)
             {
                 Console.WriteLine(_frameCount / _outputTimer.ElapsedTime.AsSeconds());
                 _outputTimer.Restart();
                 _frameCount = 0;
             }
-
             _frameCount++;
-        }
-
-        private void Initialize()
-        {
-            _timeLimit = Time.FromSeconds(2);
-            _outputTimer = new Clock();
-            _frameCount = 0;
-            _initialized = true;
-        }
-
-        public void Dispose()
-        {
-            throw new NotImplementedException();
         }
     }
 }
