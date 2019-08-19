f93c137ae3c8059e6240521c0588abfaa36d2588
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-08-19 15:42:53 +0200

    Ecs rendersystem with fps

---
+++ Engine/Systems/RenderSystem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using DefaultEcs.System;
+using SFML.Graphics;
+using SFML.System;
+
+namespace Engine.Systems
+{
+    public class RenderSystem : ISystem<RenderWindow>
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        private bool _initialized;
+        private int _frameCount;
+        private Clock _outputTimer;
+        private Time _timeLimit;
+
+        public void Update(RenderWindow state)
+        {
+            if (!_initialized)
+                Initialize();
+            if (_outputTimer.ElapsedTime > _timeLimit)
+            {
+                Console.WriteLine(_frameCount / _outputTimer.ElapsedTime.AsSeconds());
+                _outputTimer.Restart();
+                _frameCount = 0;
+            }
+
+            _frameCount++;
+        }
+
+        private void Initialize()
+        {
+            _timeLimit = Time.FromSeconds(2);
+            _outputTimer = new Clock();
+            _frameCount = 0;
+            _initialized = true;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
