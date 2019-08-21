8c43af992dfd57490e4683a499dc23f66762d783
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-08-21 20:14:45 +0200

    Add camera class

---
+++ Engine/Components/Camera.cs
@@ -0,0 +1,14 @@
+﻿using SFML.System;
+
+namespace Engine.Components
+{
+    public struct Camera
+    {
+        public Vector2f Position;
+        
+        public Camera(Vector2f position)
+        {
+            Position = position;
+        }
+    }
+}
