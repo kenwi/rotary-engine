10249a85b6019a6faf55ac277956ad1455a4f790
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-08-23 19:04:25 +0200

    Add DebugSystem and DebugWindow class for displaying runtime debug information

---
+++ Engine/Components/DebugWindow.cs
@@ -0,0 +1,19 @@
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace Engine.Components
+{
+    public struct DebugWindow
+    {
+        public DebugWindow(Vector2f position) : this()
+        {
+            Shape.Position = position;
+        }
+
+        public Shape Shape { get; }
+        public float OutlineThickness => Shape.OutlineThickness;
+        public Color FillColor => Shape.FillColor;
+        public Font Font { get; }
+        public float FontSize { get; }
+    }
+}
---
+++ Engine/Systems/DebugSystem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using DefaultEcs;
+using DefaultEcs.System;
+using Engine.Components;
+
+namespace Engine.Systems
+{
+    public class DebugSystem : AEntitySystem<DebugWindow>
+    {
+        private readonly World _world;
+
+        public DebugSystem(World world) : base(world)
+        {
+            _world = world;
+            _world.Subscribe(this);
+        }
+
+        protected override void PreUpdate(DebugWindow state)
+        {
+            base.PreUpdate(state);
+        }
+
+        protected override void Update(DebugWindow state, ReadOnlySpan<Entity> entities)
+        {
+            base.Update(state, entities);
+        }
+
+        protected override void Update(DebugWindow state, in Entity entity)
+        {
+            base.Update(state, entity);
+        }
+        
+        protected override void PostUpdate(DebugWindow state)
+        {
+            base.PostUpdate(state);
+        }
+    }
+}
