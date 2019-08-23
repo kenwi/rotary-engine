072b658fe7518bbdc2c312e6e12e20e6f73ff152
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-08-23 11:24:54 +0200

    Use Entity for window instance

--- MyExampleGame/MyEcsGame.cs
+++ MyExampleGame/MyEcsGame.cs
@@ -5,63 +5,96 @@ using SFML.System;
 using SFML.Window;
 using DefaultEcs;
 using DefaultEcs.System;
+using Engine.Managers;
+using System;
+using System.IO;
 
 namespace MyExampleGame
 {
     internal sealed class MyEcsGame
     {
-        private const float UpdateLimit = 90;
+        private const float UpdateLimit = 2;
 
         private readonly Color _clearColor;
         private readonly float _updateRate;
         private readonly ISystem<float> _updateSystem;
         private readonly ISystem<(RenderTarget, RenderStates)> _renderSystem;
-        private readonly RenderWindow _window;
+        private readonly Entity _windowEntity;
 
         private readonly World _world;
         private float _deltaTime;
         private Time _time;
+        private bool _initialized;
 
         public MyEcsGame(bool vSync, uint framerateLimit, bool fullScreen)
         {
+            _initialized = false;
             _clearColor = Color.Magenta;
             _updateRate = 1.0f / framerateLimit;
             var style = fullScreen ? Styles.Fullscreen : Styles.Default;
-            _window = new RenderWindow(new VideoMode(800, 600, 32), "DefaultTitle", style);
-            _window.Closed += (s, e) => _window.Close();
 
-            SetFrameRateLimit(vSync, framerateLimit);
+            LoadAssets();
 
             _world = new World();
             var player = _world.CreateEntity();
             player.Set<Position>(default);
+
+            _windowEntity = _world.CreateEntity();
+            _windowEntity.Set<RenderWindow>(new RenderWindow(new VideoMode(800, 600, 32), "DefaultTitle", style));
+            var window = _windowEntity.Get<RenderWindow>();
+            window.Closed += (s, e ) => window.Close();
+
+            SetFrameRateLimit(vSync, framerateLimit, window);
 
             var camera = _world.CreateEntity();
             camera.Set<Camera>(new Camera(new Vector2f()));
 
             _updateSystem = new SequentialSystem<float>(
                 new GameSystem(_world)
-                , new PlayerSystem(_world, _window)
-                , new CameraSystem(_world, _window)
+                , new PlayerSystem(_world, window)
+                , new CameraSystem(_world, window)
                 , new TileSystem(_world
-                    , tileSet: new Texture("../Assets/Tileset.png")
-                    , tileSize: new Vector2u(32, 32), mapSize: new Vector2u(32, 32))
+                    , tileSet: AssetManager.Instance.Texture.Get(AssetManagerItemName.TileSetTexture)
+                    , tileSize: new Vector2u(32, 32)
+                    , mapSize: new Vector2u(32, 32))
             );
 
             _renderSystem = new SequentialSystem<(RenderTarget, RenderStates)>(
-                new RenderSystem(_world, _window)
+                new RenderSystem(_world, window)
             );
 
             _world.Subscribe(this);
         }
 
+        public void Initialize()
+        {
+            _initialized = true;
+        }
+
+        private void LoadAssets()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var assetDirectory = currentDirectory.EndsWith("MyExampleGame") ? "../" : "../../../../";
+
+            AssetManager.Instance.Texture.Load(AssetManagerItemName.GroundTexture,
+                Path.Combine(assetDirectory, "Assets/Ground.png"));
+            AssetManager.Instance.Texture.Load(AssetManagerItemName.TreeTexture,
+                Path.Combine(assetDirectory, "Assets/Tree.png"));
+            AssetManager.Instance.Texture.Load(AssetManagerItemName.TileSetTexture,
+                Path.Combine(assetDirectory, "Assets/Tileset.png"));
+        }
+
         internal void Run()
         {
+            if (!_initialized)
+                Initialize();
+
+            var window = _windowEntity.Get<RenderWindow>();
             var clock = new Clock();
-            while (_window.IsOpen)
+            while (window.IsOpen)
             {
                 var totalTime = 0.0f;
-                while (_window.IsOpen)
+                while (window.IsOpen)
                 {
                     _time = clock.Restart();
                     _deltaTime = _time.AsSeconds();
@@ -71,34 +104,34 @@ namespace MyExampleGame
                     var updateCount = 0;
                     while (totalTime >= _updateRate && updateCount < UpdateLimit)
                     {
-                        _window.DispatchEvents();
+                        window.DispatchEvents();
                         _updateSystem.Update(_deltaTime);
                         totalTime -= _updateRate;
                         updateCount++;
                     }
 
-                    _window.Clear(_clearColor);
+                    window.Clear(_clearColor);
                     if (_renderSystem.IsEnabled)
-                        _renderSystem.Update((_window, RenderStates.Default));
-                    _window.Display();
+                        _renderSystem.Update((window, RenderStates.Default));
+                    window.Display();
                 }
             }
-            Quit();
+            Quit(window);
         }
 
-        internal void SetFrameRateLimit(bool vSync, uint framerateLimit)
+        internal void SetFrameRateLimit(bool vSync, uint framerateLimit, Window window)
         {
             if (vSync)
-                _window.SetVerticalSyncEnabled(true);
+                window.SetVerticalSyncEnabled(true);
             else
-                _window.SetFramerateLimit(framerateLimit);
+                window.SetFramerateLimit(framerateLimit);
         }
 
-        public void Quit()
+        public void Quit(Window window)
         {
             _world.Dispose();
             _updateSystem.Dispose();
-            _window.Dispose();
+            _windowEntity.Dispose();
         }
     }
 }
