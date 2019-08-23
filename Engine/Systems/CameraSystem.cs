c74a0235d241352a860fc4eef12cc940e32aedfd
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2019-08-23 11:56:57 +0200

    Add default movement test

--- Engine/Systems/CameraSystem.cs
+++ Engine/Systems/CameraSystem.cs
@@ -1,15 +1,17 @@
 ﻿using DefaultEcs;
 using DefaultEcs.System;
 using Engine.Components;
+using SFML.Graphics;
+using SFML.System;
 using SFML.Window;
 
 namespace Engine.Systems
 {
     public class CameraSystem : AEntitySystem<float>
     {
-        private readonly Window _window;
+        private readonly RenderTarget _window;
 
-        public CameraSystem(World world, Window window) 
+        public CameraSystem(World world, RenderTarget window) 
             : base(world.GetEntities().With<Camera>().Build())
         {
             _window = window;
@@ -17,7 +19,10 @@ namespace Engine.Systems
 
         protected override void Update(float state, in Entity entity)
         {
-            var camera = entity.Get<Camera>(); 
+            var camera = entity.Get<Camera>();
+            var view = _window.GetView();
+            view.Move(new Vector2f(0.0f, 0.05f));
+            _window.SetView(view);
         }
     }
 }
